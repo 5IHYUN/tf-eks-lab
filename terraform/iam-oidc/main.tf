d23c2ba57239200b0ba259d3b72a9da5b921002a
sihyun <>
2026-05-15 15:08:45 +0900

    test

--- terraform/iam-oidc/main.tf
+++ terraform/iam-oidc/main.tf
@@ -54,3 +54,4 @@ variable "github_repo" { description = "GitHub 저장소명" }
 
 output "role_arn" { value = aws_iam_role.github_actions.arn }
 output "aws_account_id" { value = data.aws_caller_identity.current.account_id }
+
